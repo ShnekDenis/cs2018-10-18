e81cbac0dc4f7ab2ae19dd9d69a2e0176d126d42
ShnekDenis <mega.kins@mail.ru>
2018-10-25 08:55:19 +0300

    Lesson 1  C complete

--- src/by/it/shnek/lesson01/Main.java
+++ src/by/it/shnek/lesson01/Main.java
@@ -2,12 +2,25 @@ package by.it.shnek.lesson01;
 
 public class Main {
     public static void main(String[] args) {
-        for (byte i = 0; i != -1; i++) {
+        for(byte i = 0; i != -1; ++i) {
             String binary = Integer.toBinaryString(i);
-            if (binary.length()>8) binary=binary.substring(binary.length()-8);
+            String octal = Integer.toOctalString(i);
+            String hexadecimal = Integer.toHexString(i);
+
+            if (binary.length() > 8) { binary = binary.substring(binary.length() - 8); }
             binary = String.format("%8s", binary).replace(" ", "0");
-            System.out.printf("%8s %4d %n",binary, i);
+            System.out.printf("%8s %4d %n", binary, i);
+
+            if (octal.length() > 8) {
+                octal = octal.substring(octal.length() - 8); }
+                octal = String.format("%8s", octal).replace(" ", "0");
+            System.out.printf("%8s %4d %n", octal, i);
+
+            if (hexadecimal.length() > 8) { hexadecimal = hexadecimal.substring(hexadecimal.length() - 8); }
+            hexadecimal = String.format("%8s", hexadecimal).replace(" ", "0");
+            System.out.printf("%8s %4d %n", hexadecimal, i);
 
         }
+
     }
 }
--- src/by/it/shnek/lesson02/TaskA1.java
+++ src/by/it/shnek/lesson02/TaskA1.java
@@ -9,7 +9,7 @@ Hello world!
 */
 
 class TaskA1 {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
+        }
 }
